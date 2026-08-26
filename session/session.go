f18e7532f2f4655f4bfea54a9262cbf20e93b987
Maxime de Courchelle <maximebtz@hotmail.com>
2026-08-26 13:06:35 +0200

    refactor: promote session windowing constants
    sessionDuration and sessionTimeout never varied per GameSession
    instance; promoted from struct fields set on every NewSession call to
    package constants.

--- session/session.go
+++ session/session.go
@@ -7,12 +7,15 @@ import (
 	"github.com/mxdc/cs2-discord-bot/leetify"
 )
 
+const (
+	sessionDuration = 3*time.Hour + 15*time.Minute
+	sessionTimeout  = 3*time.Hour + 30*time.Minute
+)
+
 type GameSession struct {
 	Matches           []leetify.Game
 	LastMatchEndTime  time.Time
 	LastDetectionTime time.Time
-	sessionDuration   time.Duration
-	sessionTimeout    time.Duration
 	IsFresh           bool
 	debugMode         bool
 }
@@ -22,8 +25,6 @@ func NewSession(game leetify.Game, detectedAt time.Time, debugMode bool) *GameSe
 		Matches:           []leetify.Game{game},
 		LastMatchEndTime:  game.FinishedAt(),
 		LastDetectionTime: detectedAt,
-		sessionDuration:   3*time.Hour + 15*time.Minute,
-		sessionTimeout:    3*time.Hour + 30*time.Minute,
 		IsFresh:           false,
 		debugMode:         debugMode,
 	}
@@ -46,16 +47,16 @@ func (s *GameSession) AddMatch(game leetify.Game, detectedAt time.Time) {
 
 func (s *GameSession) IsSessionTimeout() bool {
 	if s.debugMode {
-		return time.Since(s.LastMatchEndTime) > s.sessionTimeout
+		return time.Since(s.LastMatchEndTime) > sessionTimeout
 	}
 
-	return time.Since(s.LastDetectionTime) > s.sessionTimeout
+	return time.Since(s.LastDetectionTime) > sessionTimeout
 }
 
 func (s *GameSession) IsMatchPartOfSession(game leetify.Game) bool {
 	diff := game.FinishedAt().Sub(s.LastMatchEndTime).Abs()
 
-	return diff <= s.sessionDuration
+	return diff <= sessionDuration
 }
 
 func (s *GameSession) IsMatchBeforeCurrentSession(game leetify.Game) bool {
